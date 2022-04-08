e8b238b64ca39be66bf3f061d084ae2a6aaf73bb
Sepooo <nicolopriano23@gmail.com>
2022-04-08 11:16:11 +0200

    FUNZIONA

--- Labo1/labo1.c
+++ Labo1/labo1.c
@@ -2,68 +2,110 @@
 #include <stdio.h>
 #include <string.h>
 #include <pthread.h>
+#include <time.h>
 
-#define RAND_MAX = 9;
+pthread_barrier_t barrier;
+int M,N,P, N_THREADS = 3;
 
-void ins (int row, int column, int matrix[row][column]);
-void printMatrix(int row, int column, int matrix[row][column]);
+void ins (int row, int column, int** matrix);
+int** createArray(int m, int n);
+void destroyArray(int** arr);
+void printMatrix(int row, int column, int** matrix);
 
-int main()
-{
-    int M = 6;
-    int N = 3;
-    int P = 2;
+typedef struct matrix {
+    int rows; //numero righe
+    int cols; //num colonne
+    int **data;
+}matrix;
 
-    int A[M][N];
-    int B[N][P];
-    int C[P][M];
-    int R[M][P];
-    //int Q[P][P];
+struct matrix A;
+struct matrix B;
+struct matrix R;
 
-    ins(M,N,A);
-    printMatrix(M,N,A);
+void *mul(void *arg) {
+    int indexRow = *((int *)arg);
+    free(arg);
+    printf("indexRow: %d\n", indexRow);
+    int moltiplicazione = 0;
     
-    ins(N,P,B);
-    printMatrix(N,P,B);
-
-    ins(P,M,C);
-    printMatrix(P,M,C);
-
-    //print(P,M,C);
-
-    int sum;
-    for(int i=0; i < M; i++) {
-        for(int k=0; k < P; k++) {
-            sum = 0;
-            for(int j=0; j < N; j++) {
-                printf("%d * %d  ", A[i][j], B[j][k]);
-                sum += A[i][j] * B[j][k];
-            }
-            printf("/n ----%d  ", sum);
-            R[i][k] = sum;
+    for(int i=0; i<B.cols; i++) {
+        for(int j=0; j<B.rows; j++) {
+            moltiplicazione += A.data[indexRow][j]*B.data[j][i];
         }
-    }
-    printMatrix(M,P,R);
 
-    int Q[P][P];
+        R.data[indexRow][i] = moltiplicazione;
+        moltiplicazione = 0; 
+    }  
 
-    for(int i=0; i < P; i++) {
-        for(int k=0; k < P; k++) {
-            int sum = 0;
-            for(int j=0; j < M; j++) {
-                printf("%d * %d  ", C[i][j], R[j][k]);
-                sum += C[i][j] * R[j][k];
-            }
-            printf("/n ----%d  ", sum);
-            Q[i][k] = sum;
-        }
-    }
-
-    printf("\n");
-    printMatrix(P,P,Q);
+    pthread_barrier_wait(&barrier);
+    printf("RUNNING\n");
 }
 
-void ins (int row, int column, int matrix[row][column])
+int main() {
+
+    //L'utente inserisce numero righe e colonne della prima matrice (A)
+    printf("Inserire num righe matrice A: ");
+    scanf("%d", &M); 
+    printf("Inserire num colonne matrice A: ");
+    scanf("%d", &N);
+    printf("Inserire num colonne matrice B: "); 
+    scanf("%d", &P);
+
+    pthread_barrier_init(&barrier, NULL, M+1);
+    A.data = createArray(M,N); //creo la matrice A
+    A.rows = M;
+    A.cols = N;
+    
+    B.data = createArray(N,P);
+    B.rows = N;
+    B.cols = P;
+
+    ins(A.rows, A.cols, A.data);
+    ins(B.rows, B.cols, B.data);
+    printMatrix(A.rows, A.cols, A.data);
+    printMatrix(B.rows, B.cols, B.data);
+
+    R.data = createArray(M,P);
+    R.rows = M;
+    R.cols = P;
+
+    int num_threads = M;
+    pthread_t tid[M];
+
+    for(int i=0; i < num_threads; i++) {
+        int* k= malloc(sizeof(int));
+        *k=i;
+        pthread_create(&tid[i], NULL, &mul,  (void*) k);
+    }
+
+    pthread_barrier_wait(&barrier);
+    for(int i=0; i<num_threads; i++) {
+        pthread_join(tid[i], NULL);
+    }
+
+    pthread_barrier_destroy(&barrier);
+    printMatrix(M,P,R.data);
+    destroyArray(A.data);
+    destroyArray(B.data);
+    destroyArray(R.data);
+}
+
+
+int** createArray(int m, int n) {
+    int* values = calloc(m*n, sizeof(int));
+    int** rows = malloc(m*sizeof(int*));
+    for (int i=0; i<m; ++i) {
+        rows[i] = values + i*n;
+    }
+    return rows;
+}
+
+void destroyArray(int** arr) {
+    free(*arr);
+    free(arr);
+}
+
+void ins (int row, int column, int** matrix)
 {
     for(int i = 0; i < row; i++) {
         for(int j = 0; j < column; j++) {
@@ -72,7 +114,7 @@ void ins (int row, int column, int matrix[row][column])
     }
 }
 
-void printMatrix(int row, int column, int matrix[row][column])
+void printMatrix(int row, int column, int** matrix)
 {
     for(int i = 0; i < row; i++) {
         printf("|   ");
@@ -83,4 +125,3 @@ void printMatrix(int row, int column, int matrix[row][column])
     }
     printf("\n\n");
 }
-//pippo
