264813c37868cf3a59a67e0e0fbfdaeb4da5fb52
Sepooo <nicolopriano23@gmail.com>
2022-04-08 13:57:11 +0200

    aggiunti commenti

--- Labo1/labo1.c
+++ Labo1/labo1.c
@@ -5,34 +5,39 @@
 #include <time.h>
 
 pthread_barrier_t barrier;
-int M,N,P;
+int M,N,P; // Numero righe e colonne delle matrici
 
 
-void ins (int row, int column, int** matrix);
-int** createArray(int m, int n);
-void destroyArray(int** arr);
-void printMatrix(int row, int column, int** matrix);
+void ins (int row, int column, int** matrix); // inserisce valori random da 0 a 9 nella matrice
+int** createArray(int m, int n); // crea l'array bidimensionale che conterrà la matrice
+void destroyArray(int** arr); // elimina la matrice, libera la memoria 
+void printMatrix(int row, int column, int** matrix); // stampa la matrice
 
-typedef struct matrix {
-    int rows; //numero righe
-    int cols; //num colonne
-    int **data;
+typedef struct matrix { // Ogni matrice è una struct 
+    int rows; // numero righe
+    int cols; // num colonne
+    int **data; // array bidimensionale che contiene la matrice
 }matrix;
 
+// A B e R sono matrici globali, in modo che possano essere viste anche dai thread
 struct matrix A;
 struct matrix B;
 struct matrix R;
 
 void *mul(void *arg) {
-    int indexRow = *((int *)arg);
-    free(arg);
-    printf("indexRow: %d\n", indexRow);
-    int moltiplicazione = 0;
+    int indexRow = *((int *)arg); // ho passato *k al thread e l'ho salvato in indexRow
+    free(arg); // libero k* (la zona di memoria puntata da k)
+    printf("indexRow: %d\n", indexRow); // serve per il debug
+    int moltiplicazione = 0; // conterrà il valore di una cella della matrice R (risultato)
     
-    for(int i=0; i<B.cols; i++) {
+    // Moltiplico la riga indexRow di A per tutte le colonne di B 
+    for(int i=0; i<B.cols; i++) { 
         for(int j=0; j<B.rows; j++) {
             moltiplicazione += A.data[indexRow][j]*B.data[j][i];
         }
+
+        // salvo il risultato nella riga indexRow di R, al termine del ciclo 
+        // avrò calcolato tutta la riga indexRow di R
 
         R.data[indexRow][i] = moltiplicazione;
         moltiplicazione = 0; 
@@ -44,7 +49,7 @@ void *mul(void *arg) {
 
 int main() {
 
-    //L'utente inserisce numero righe e colonne della prima matrice (A)
+    // L'utente inserisce numero righe e colonne della prima matrice (A)
     printf("Inserire num righe matrice A: ");
     scanf("%d", &M); 
     printf("Inserire num colonne matrice A: ");
@@ -52,12 +57,12 @@ int main() {
     printf("Inserire num colonne matrice B: "); 
     scanf("%d", &P);
 
-    pthread_barrier_init(&barrier, NULL, M+1);
-    A.data = createArray(M,N); //creo la matrice A
+    pthread_barrier_init(&barrier, NULL, M); // inizializzo la barriera
+    A.data = createArray(M,N); // creo la matrice A
     A.rows = M;
     A.cols = N;
     
-    B.data = createArray(N,P);
+    B.data = createArray(N,P); // creo la matrice B
     B.rows = N;
     B.cols = P;
 
@@ -66,20 +71,24 @@ int main() {
     printMatrix(A.rows, A.cols, A.data);
     printMatrix(B.rows, B.cols, B.data);
 
-    R.data = createArray(M,P);
+    R.data = createArray(M,P); // creo la matrice R
     R.rows = M;
     R.cols = P;
 
-    int num_threads = M;
+    int num_threads = M; // Verranno creati tanti thread quante sono le righe della matrice A
     pthread_t tid[M];
 
     for(int i=0; i < num_threads; i++) {
-        int* k= malloc(sizeof(int));
-        *k=i;
-        pthread_create(&tid[i], NULL, &mul,  (void*) k);
+        int* k= malloc(sizeof(int)); 
+        *k=i; 
+        pthread_create(&tid[i], NULL, &mul,  (void*) k); 
+        // creo il thread passando k (sarebbe come passare i)
+        // Se passassi direttamente i, i thread vedrebbero valori uguali o incasinati,
+        // in questo modo salvo il valore di i in una zona di memoria puntata da k
+        // il thread salverà questo valore e farà la free() di k*, in modo che 
+        // nel prossimo ciclo a k* verrà assengato il valore di i incrementato
     }
 
-    pthread_barrier_wait(&barrier);
     for(int i=0; i<num_threads; i++) {
         pthread_join(tid[i], NULL);
     }
