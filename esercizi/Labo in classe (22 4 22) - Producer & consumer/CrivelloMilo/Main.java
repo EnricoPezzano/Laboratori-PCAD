be3f1abbafed103bdec95693bedc3d5d5b5c56c7
Enrico Pezzano <53240167+EnricoPezzano@users.noreply.github.com>
2022-04-28 10:27:53 +0200

    enrico's script push

--- esercizi/Labo in classe (22 4 22) - Producer & consumer/CrivelloMilo/Main.java
+++ esercizi/Labo in classe (22 4 22) - Producer & consumer/CrivelloMilo/Main.java
@@ -2,19 +2,22 @@ package CrivelloMilo;
 import java.util.*;
 
 public class Main {
-  public static void main(String[] args){
-    Scanner input = new Scanner(System.in);
-    System.out.print("\nCalcola i numeri primi fino a : ");
-    int N = input.nextInt();
-    
-    Crivello crivello = new Crivello(3);
-    crivello.start();
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		System.out.print("\nCalcola i numeri primi fino a : ");
+		int N = input.nextInt();
 
-    System.out.println("\n");
+		Crivello crivello = new Crivello(3);
+		crivello.start();
 
-    for(int i = 3; i <= N; i++)if( i%2 != 0)crivello.push(i);
-    crivello.push(-1);
-    crivello.interrupt();
-    input.close();
-  }  
+		System.out.println("\n");
+
+		for(int i = 3; i <= N; i++)
+			if( i%2 != 0)
+				crivello.push(i);
+
+		crivello.push(-1);
+		crivello.interrupt();
+		input.close();
+	}
 }
