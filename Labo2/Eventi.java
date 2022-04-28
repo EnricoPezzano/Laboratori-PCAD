516f75cf5513d0678472f0b6e37e9f9b96f8eb68
Enrico Pezzano <53240167+EnricoPezzano@users.noreply.github.com>
2022-04-28 11:06:49 +0200

    enrico's script push

---
+++ Labo2/Eventi.java
@@ -0,0 +1,28 @@
+public class Eventi{
+   private Evento Lista;
+
+
+   public Eventi(){
+      
+   }
+
+   public void Crea(String NomeEvento, int Posti){
+      
+   }
+
+   public void Aggiungi(String NomeEvento, int Posti){
+      
+   }
+
+   public void Prenota(String NomeEvento, int Posti){
+      
+   }
+
+   public void ListaEventi(){
+      
+   }
+
+   public void Chiudi(String NomeEvento){
+      
+   }
+}
---
+++ Labo2/Evento.java
@@ -0,0 +1,6 @@
+public class Evento{
+   private String NomeEvento;
+   private int Posti;
+
+   
+}
