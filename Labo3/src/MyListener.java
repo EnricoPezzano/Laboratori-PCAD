95d4feabe39659a0228e74282207041fbf69e327
Enrico Pezzano <53240167+EnricoPezzano@users.noreply.github.com>
2022-05-17 13:26:42 +0200

    enrico's script push

---
+++ Labo3/src/MyListener.java
@@ -0,0 +1,19 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MyListener implements ActionListener {
+
+    private GUI gui;
+    private MyWorker worker;
+
+    public MyListener(GUI gui) {
+        this.gui = gui;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ev) {
+        gui.step.setEnabled(false);
+        worker = new MyWorker(gui);
+        worker.execute();
+    }
+}
