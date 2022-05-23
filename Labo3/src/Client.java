ecee323e8a675eb086ffd0e6e1485ab23072cae4
Enrico Pezzano <53240167+EnricoPezzano@users.noreply.github.com>
2022-05-23 13:51:16 +0200

    enrico's script push

--- Labo3/src/Client.java
+++ Labo3/src/Client.java
@@ -1,3 +1,40 @@
-public class Client{
-   
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class Client {
+	
+	// driver code
+	public static void main(String[] args)
+	{
+		// establish a connection by providing host and port number
+		try (Socket socket = new Socket("localhost", 1234)) {
+			
+			// writing to server
+			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+			// reading from server
+			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+			// object of scanner class
+			Scanner sc = new Scanner(System.in);
+			String line = null;
+
+			while (!"exit".equalsIgnoreCase(line)) {
+				line = sc.nextLine(); // reading from user
+
+				// sending the user input to server
+				out.println(line);
+				out.flush();
+
+				// displaying server reply
+				System.out.println("Server replied "+ in.readLine());
+			}
+			
+			sc.close(); // closing the scanner object
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
