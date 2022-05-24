5525b1c7e4292cb5f6c57f59e75d491e26dfab95
Enrico Pezzano <53240167+EnricoPezzano@users.noreply.github.com>
2022-05-24 16:46:06 +0200

    enrico's script push

--- Labo3/src/GUI/GUI.java
+++ Labo3/src/GUI/GUI.java
@@ -1,36 +1,38 @@
-package GUI;
-
 import javax.swing.*;
 
 public class GUI extends JFrame {
 
-    private static final long serialVersionUID = 1L;
-	JButton step;
-    JLabel  statusLabel;
-    int counter;
+   private static final long serialVersionUID = 1L;
+   JButton step;
+   JLabel  statusLabel;
+   int counter;
     
-    public GUI() {
-        super("Swing");
-        counter=0;
-        step = new JButton("Step");
-        statusLabel = new JLabel("0");
-        MyListener step_handler  = new MyListener(this);
-        step.addActionListener(step_handler);
-        JPanel Panel = new JPanel();
-        Panel.add(step);
-        Panel.add(statusLabel);
-        getContentPane().add(Panel);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setVisible(true);
-        pack();
-    }
+   // campi nome e posti
+   // tasto prenota
 
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                new GUI();
-            }
-        });
-    }
+
+   public GUI() {
+      super("Swing");
+      counter=0;
+      step = new JButton("Step");
+      statusLabel = new JLabel("0");
+      MyListener step_handler  = new MyListener(this);
+      step.addActionListener(step_handler);
+      JPanel Panel = new JPanel();
+      Panel.add(step);
+      Panel.add(statusLabel);
+      getContentPane().add(Panel);
+      setDefaultCloseOperation(EXIT_ON_CLOSE);
+      setVisible(true);
+      pack();
+   }
+
+   public static void main(String[] args) {
+      SwingUtilities.invokeLater(new Runnable() {
+         @Override
+         public void run() {
+               new GUI();
+         }
+      });
+   }
 }
